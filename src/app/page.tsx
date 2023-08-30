51f7c73649db7653520b05bca43b8475785c2362
andres <afmo2@byu.edu>
2023-08-30 15:02:20 -0600

    text container and button styled and margins added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,12 +10,11 @@ export default function Home() {
           fill
           className='object-cover opacity-70'
         />
-        <div className='absolute bottom-[100px] inset-x-0 flex_center flex-col'>
 
-
+        <div className='absolute bottom-48 inset-x-0 flex_center flex-col'>
           {/*Name and Role*/}
-          <h1 className='text-secondary_lightgray name font-oriya'>Andres Molina</h1>
-          <h4 className='text-secondary_darkgray name__role font-oriya'>Software Developer</h4>
+          <p className='text-secondary_lightgray name font-oriya'>Andres Molina</p>
+          <p className='text-secondary_darkgray name__role font-oriya'>Software Developer</p>
 
           {/*Projects Botton -> Should Redirect to more projects*/}
           <button className='project_button border-solid border-2 border-comp_green rounded-xl'>
